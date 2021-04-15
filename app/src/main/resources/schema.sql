ed508a3b3f470e814e9235f99664619541ff3430
carlcidromero <carl.romero@revature.net>
2021-04-15 14:53:31 -0700

    setup db instead, servlets next

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -3,8 +3,6 @@ DROP DATABASE IF EXISTS ghud;
 
 CREATE DATABASE ghud;
 
-DROP TABLE IF EXISTS headspace CASCADE;
-
 CREATE TABLE thought (
     id SERIAL PRIMARY KEY,
     tldr TEXT NOT NULL
@@ -15,56 +13,64 @@ CREATE TABLE soul (
     name TEXT NOT NULL
 );
 
-TRUNCATE TABLE thought;
-
-CREATE TABLE thought (
-    id SERIAL PRIMARY KEY,
-    tldr TEXT NOT NULL
-);
-
 CREATE TABLE vibe (
     id SERIAL PRIMARY KEY,
+    soul_id INT NOT NULL,
     tldr TEXT NOT NULL,
-    purpose TEXT NOT NULL
+    purpose TEXT NOT NULL,
+    
+    CONSTRAINT fk_vibe_soul
+        FOREIGN KEY(soul_id)
+            REFERENCES soul(id)
+            ON DELETE CASCADE
 );
-
-ALTER TABLE vibe ADD FK_vibe_soul NUMBER NOT NULL FOREIGN KEY REFERENCES soul(id); 
 
 CREATE TABLE headspace (
     id SERIAL PRIMARY KEY,
-    location TEXT NOT NULL,
-    frame TEXT NOT NULL,
+    thought_id INT NOT NULL,
+    vibe_id INT,
+    location TEXT NOT NULL
+        CHECK (
+            (location = 'love') or
+            (location = 'live') or
+            (location = 'learn') or
+            (location = 'earn')
+        ),
+    frame TEXT NOT NULL
+        CHECK (
+            (frame = 'top of mind') or
+            (frame = 'front and center') or
+            (frame = 'bottom line')
+        ),
 
-    FK_headspace_thought NUMBER NOT NULL
-        FOREIGN KEY REFERENCES thought(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-        
-    FK_headspace_vibe NUMBER NOT NULL
-        FOREIGN KEY REFERENCES vibe(id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    CONSTRAINT fk_headspace_thought
+        FOREIGN KEY(thought_id)
+            REFERENCES thought(id)
+            ON DELETE CASCADE,
+    
+    CONSTRAINT fk_headspace_vibe
+        FOREIGN KEY(vibe_id)
+            REFERENCES vibe(id)
+            ON DELETE CASCADE
 );
 
--- Data Manipulation Language (DML: CRUD)
-INSERT INTO soul (name) VALUES ('jenny');
+INSERT INTO thought (tldr)
+    VALUES
+        ('gotta do my taxes'),
+        ('finish project1'),
+        ('finish been saiyan song'),
+        ('finish peace of pi song')
+;
 
-INSERT INTO vibe (tldr, purpose, FK_vibe_soul)
-    VALUES ('say hi', 'check-in', 1);
+INSERT INTO soul (name)
+    VALUES
+        ('stretch ego'),
+        ('seth from above'),
+        ('chanarah'),
+        ('steve')
+;
 
-INSERT INTO thought VALUES ('hmm...');
-
-UPDATE vibe SET tldr = 'say hello' WHERE id = 1;
-
-DELETE thought WHERE tldr LIKE 'hmm...';
-
--- Data Control Language (DCL)
-GRANT SELECT, INSERT, UPDATE, DELETE, ALTER, REFERENCES ON thought TO project1;
-GRANT SELECT, INSERT, UPDATE, DELETE, ALTER, REFERENCES ON vibe TO project1;
-GRANT SELECT, INSERT, UPDATE, DELETE, ALTER, REFERENCES ON soul TO project1;
-GRANT SELECT, INSERT, UPDATE, DELETE, ALTER, REFERENCES ON headspace TO project1;
-
-REVOKE ALL, ALTER, REFERENCES ON thought FROM public;
-REVOKE ALL, ALTER, REFERENCES ON vibe FROM public;
-REVOKE ALL, ALTER, REFERENCES ON soul FROM public;
-REVOKE ALL, ALTER, REFERENCES ON headspace FROM public;
+INSERT INTO headspace (thought_id, location, frame)
+    VALUES
+        (1, 'live', 'top of mind')
+;
